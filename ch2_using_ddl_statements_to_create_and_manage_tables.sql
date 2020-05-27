2b7cf2ad038a2c1835aa4890bc437221cb8b2669
almazer-dev <57685622+almazer-dev@users.noreply.github.com>
2020-05-27 00:03:21 -0300

    testes com tipo Number(n,m)

--- ch2_using_ddl_statements_to_create_and_manage_tables.sql
+++ ch2_using_ddl_statements_to_create_and_manage_tables.sql
@@ -16,3 +16,38 @@ CREATE TABLE "Company Employees" (
 
 SELECT * FROM "Company Employees";
 SELECT * FROM "company employees"; --FAIL
+
+-- Data Types for columns
+
+-- Number(n,m)
+select cast(0.0001 as number(2,5)) as num, 
+       to_char(cast(0.0001 as number(2,5))) as cnum,
+       dump(cast(0.0001 as number(2,5))) as dmp from dual;
+       
+select cast(.0000123 as number(6, 9)) from dual;       
+select cast(.000012345 as number(6, 9)) from dual;
+select cast(.123456 as number(3, 4)) from dual; -- ERROR
+select cast(0.013579 as number(3, 4)) from dual;
+
+select cast(0.000123 as number(2,5)) from dual;
+select cast(0.00123 as number(2,5)) from dual; -- ERROR
+select cast(0.000123 as number(2,6)) from dual;
+select cast(0.000999 as number(2,5)) from dual; -- ERROR
+
+select cast(4.56 as number) from dual;
+select cast(4.56 as number(2)) from dual;
+select cast(4.56 as number(5,2)) from dual;
+select cast(4.5678 as number(5,2)) from dual;
+select cast(10.59 as number(4,2)) from dual;
+select cast(10.59 as number(4,1)) from dual;
+select cast(10.59 as number(3,2)) from dual; -- ERROR
+
+select cast(1059.34 as number(5,-2)) from dual;
+select cast(999.34 as number(3,-2)) from dual;
+
+select cast(999.34 as number(3,-1)) from dual;
+select cast(9999.34 as number(3,-2)) from dual;
+select cast(99999.34 as number(3,-3)) from dual;
+select cast(999.34 as number(3,-3)) from dual;
+select cast(99.34 as number(3,-3)) from dual;
+select cast(999.34 as number(3,-2)) from dual;
