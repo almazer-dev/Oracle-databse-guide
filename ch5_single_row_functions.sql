a6bd1373687dcc2498954d1eafee81f7f73bbec7
Ângelo Mazer <rangelop@hotmail.com>
2020-09-02 00:13:34 -0300

    exemplos com funcoes de data

--- ch5_single_row_functions.sql
+++ ch5_single_row_functions.sql
@@ -57,5 +57,28 @@ SELECT SOUNDEX('Angelo'), soundex('angelica') from dual;
 SELECT CEIL(4.01) FROM DUAL;               
 select floor(3.99) from dual;	
 
-select round(12.355143, 2), round(259.99, -1), round(259.99) from dual;               
-select trunc(12.355143, 2), trunc(259.99, -1), trunc(259.99) from dual;                              
+select round(12.355234, 2), round(259, -1), round(259.99), round(254,-1) from dual;               
+select trunc(12.355143, 2), trunc(259.99, -1), trunc(259.99) from dual;      
+
+select remainder(9,3), remainder(10,3), remainder(11,3) from dual; -- retorna -1 se múltiplo mais próximo de n1 for maior que n1
+select mod(9,3), mod(10,3), mod(11,3) from dual;
+
+
+select sysdate from dual;
+
+select sysdate from dual;
+select sysdate, round(sysdate, 'MM'), round(to_date('16/09/20', 'dd/mm/yy'), 'MM'), round(to_date('14/09/20', 'dd/mm/yy'), 'MM')  from dual;
+select round(sysdate, 'RR') from dual;
+select to_char(sysdate, 'dd/mm/yy hh24:mi:ss'), to_char(round(sysdate), 'dd/mm/yy hh24:mi:ss') from dual;
+select to_char(to_date('01/09/20 12:00:00', 'dd/mm/yy hh24:mi:ss'), 'dd/mm/yy hh24:mi:ss') data_meio_dia, 
+       to_char(round(to_date('01/09/20 12:00:00', 'dd/mm/yy hh24:mi:ss')), 'dd/mm/yy hh24:mi:ss') data_meio_dia_round from dual;
+
+select sysdate, trunc(sysdate, 'MM'), trunc(sysdate, 'RR') from dual;
+select to_char(to_date('01/09/20 12:00:00', 'dd/mm/yy hh24:mi:ss'), 'dd/mm/yy hh24:mi:ss') data_meio_dia, 
+       to_char(trunc(to_date('01/09/20 12:00:00', 'dd/mm/yy hh24:mi:ss')), 'dd/mm/yy hh24:mi:ss') data_meio_dia_trunc from dual;
+
+SELECT NEXT_DAY('01/09/20', 'sábado') from dual;
+
+select last_day('01/02/20'), last_day('01/02/21') from dual;
+
+select add_months('31/JAN/17', 1), add_months('01/NOV/17', 4), ADD_MONTHS('28/FEV/17', -1), ADD_MONTHS('01/FEV/17', -1) FROM DUAL;
