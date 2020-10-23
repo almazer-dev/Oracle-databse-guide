c38f5fcf36495acfc11bee2177a596f3ac05eadb
Ângelo Mazer <rangelop@hotmail.com>
2020-10-23 10:18:55 -0300

    exemplos de formatos to_number

--- ch6_conversion_functions.sql
+++ ch6_conversion_functions.sql
@@ -22,20 +22,36 @@ select to_number('$99,999.12', '$99,999.99') from dual;
 select to_number('$99,999.12', '$9,999.99') from dual; --ORA-01722: número inválido
 select to_number('$099,999.12', '$099,999.99') from dual;
 
+select to_number('007,3', '009D9') from dual;
+select to_number('07,3', '009D9') from dual; --ORA-01722: número inválido
+
+--B
 select to_number(' 12', '99') from dual; --?
 select to_number('2', 'B999') from dual; --?
 select to_number('  12', '99') from dual; --?
 
-
+--C
 select to_number('BRL200', 'C999') from dual;
 
+--D
 select to_number('123.1234', '999D9999') from dual;--ORA-01722: número inválido (Se o separador de decimal for "," e o agrupador for ".")
 
+--EEE
+
+--G
+select to_number('444,123.1234', '999G999D9999', 'nls_numeric_characters=''.,''' ) from dual;
+
+--L
+select to_number('R$4','L99') from dual;
+
+--MI
 select to_number('1-', '9MI') from dual;
 select to_number('1 ', '9MI') from dual;
 select to_number('1 ', '9') from dual; --ORA-01722: número inválido
 
+--pr
 select to_number('<1>', '99PR') from dual;
 select to_number('1', '99PR') from dual;
+select to_number('<12,4>', '99D9pr') from dual;
 
-select to_number('444,123.1234', '999G999D9999', 'nls_numeric_characters=''.,''' ) from dual;
+select to_number('123.1234', '999D9999', 'nls_numeric_characters=''.,''') from dual;
